821b1d0933671e5b5f6bc8e2c8d283c72944f27f
xsoly1-boop <xsoly.1@gmail.com>
2026-06-20 00:12:24 -0600

    feat: agregar soporte para Android con Capacitor

---
+++ android/app/src/main/java/com/dj/interactive/platform/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dj.interactive.platform;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dj.interactive.platform',
+  appName: 'DJ Control Panel',
+  webDir: 'dist'
+};
+
+export default config;
