f5c84763a7aff74d3adfb41f34f6e93fc02e90e6
xsoly1-boop <xsoly.1@gmail.com>
2026-06-20 21:56:00 -0600

    fix: cambiar visibilidad de onResume a public en MainActivity

--- android/app/src/main/java/com/dj/interactive/platform/MainActivity.java
+++ android/app/src/main/java/com/dj/interactive/platform/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         enableImmersiveMode();
     }
